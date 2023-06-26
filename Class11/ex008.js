97e6071fb765c49aadbaf82d68f016808f779eb8
pfrizo <pedrofrizo@hotmail.com>
2023-06-26 20:43:37 -0300

    Classes 10 and 11
    Exercises from classes 10 and 11

---
+++ Class11/ex008.js
@@ -0,0 +1,8 @@
+var speed = 51.2
+console.log(`The speed is ${speed} km/h`)
+
+if(speed > 60){
+    console.log('You have exceeded the speed limit. MULT!')
+}
+
+console.log('Always drive wearing a seat belt!')
---
+++ Class11/ex009.js
@@ -0,0 +1,9 @@
+var country = 'Brazil'
+
+console.log(`Living in ${country}`)
+
+if(country != 'Brazil'){
+    console.log("You're foreigner")
+} else {
+    console.log("You're Brazilian")
+}
