78b2cd53d38d8a92b1915cff9900f98577722718
pfrizo <pedrofrizo@hotmail.com>
2023-07-09 21:08:52 -0300

    class 14 exercises - ex016
    class 14 exercises - ex016 completed; ex017 started.

---
+++ Class14Exs/ex016/script.js
@@ -0,0 +1,28 @@
+function count(){
+    var starttxt = document.getElementById('start')
+    var endtxt = document.getElementById('end')
+    var steptxt = document.getElementById('steps')
+    var start = Number(starttxt.value)
+    var end = Number(endtxt.value)
+    var step = Number(steptxt.value)
+    var res = document.getElementById('res')
+    var p = document.createElement('p')
+    
+    if(starttxt.value.length == 0 || endtxt.value.length == 0){
+        res.innerHTML = 'Impossible to count!'
+    } else {
+        if (steptxt.value.length == 0 || step == 0){
+            alert('Invalid Steps! Considering 1 step')
+            step = 1
+        }
+        res.innerHTML = 'Counting:'
+
+        while(start <= end){
+            p.innerHTML += `${start} &#128073;`
+            start += step
+        }
+    
+        p.innerHTML += '&#127937;'
+        res.appendChild(p)
+    }
+}
